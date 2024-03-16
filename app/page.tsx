b8e84e6a3527068a3ecdd5d7cea9194e2c892f84
gayashan4lk <gayashan4lk@gmail.com>
2024-03-16 07:26:48 +0530

    center items

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import { HomeComponent } from '@/components/HomeComponent';
+import { HomeComponent } from '@/components/HomeComponent'
 
 export default function Home() {
   return (
-    <div className='bg-gray-200'>
-      <div className='p-4 mx-auto max-w-96'>This div is centered</div>
-    </div>
-  );
+    <>
+      <div className="my-3 bg-gray-200">
+        <div className="mx-auto max-w-96 p-4">This element is centered using margin:auto</div>
+      </div>
+      <div className="my-3 flex h-28 justify-center bg-blue-200">
+        <div className="p-4">This element is centered using Flex box</div>
+      </div>
+      <div className="my-3 grid h-28 bg-orange-200">
+        <div className="mx-auto p-4">This element is centered using Grid and margin:auto</div>
+      </div>
+      <div className="my-3 grid h-28 place-items-center bg-green-200">
+        <div className="p-4">This element is centered using Grid</div>
+      </div>
+    </>
+  )
 }
