566625d0786cc9e49c59e3ca6028e22cda70d68e
gayashan4lk <gayashan4lk@gmail.com>
2024-03-16 07:58:01 +0530

    add nav bar

---
+++ app/center-an-item/page.tsx
@@ -0,0 +1,18 @@
+export default function Page() {
+  return (
+    <>
+      <div className="my-3 bg-gray-200">
+        <div className="mx-auto max-w-96 p-4">This element is centered using margin:auto</div>
+      </div>
+      <div className="my-3 flex h-28 justify-center bg-blue-200">
+        <div className="p-4">This element is centered using Flex box</div>
+      </div>
+      <div className="my-3 grid h-28 bg-orange-200">
+        <div className="mx-auto p-4">This element is centered using Grid and margin:auto</div>
+      </div>
+      <div className="my-3 grid h-28 place-items-center bg-green-200">
+        <div className="p-4">This element is centered using Grid</div>
+      </div>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,6 @@
 import { HomeComponent } from '@/components/HomeComponent'
+import { NavBar } from '@/components/NavBar'
 
 export default function Home() {
-  return (
-    <>
-      <div className="my-3 bg-gray-200">
-        <div className="mx-auto max-w-96 p-4">This element is centered using margin:auto</div>
-      </div>
-      <div className="my-3 flex h-28 justify-center bg-blue-200">
-        <div className="p-4">This element is centered using Flex box</div>
-      </div>
-      <div className="my-3 grid h-28 bg-orange-200">
-        <div className="mx-auto p-4">This element is centered using Grid and margin:auto</div>
-      </div>
-      <div className="my-3 grid h-28 place-items-center bg-green-200">
-        <div className="p-4">This element is centered using Grid</div>
-      </div>
-    </>
-  )
+  return <div>Welcome to tailwind component!</div>
 }
